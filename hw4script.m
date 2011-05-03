398a71f24ff561f556a887f914117566609f3ffe
Iain Crosby <ifcrosby@gmail.com>
2011-05-03 15:50:57 -0400

    added some text to explain what was going on in script file

--- hw4script.m
+++ hw4script.m
@@ -1,25 +1,51 @@
 test_blueTicket
 
+disp('Tests the code for blueTicket.m')
+
 test_cigarParty
+
+disp('Tests the code for cigarParty.m')
 
 test_close10
 
+disp('Tests the code for close10.m')
+
 test_everyNth
+
+disp('Tests the code for everyNth.m')
 
 test_in3050
 
+disp('Tests the code for in3050.m')
+
 test_intMax
+
+disp('Tests the code for intMax.m')
 
 test_lastDigit
 
+disp('Tests the code for lastDigit.m')
+
 test_lessby10
+
+disp('Tests the code for lessby10.m')
 
 test_nearOnes
 
+disp('Tests the code for nearOnes.m')
+
 test_parrot_trouble
+
+disp('Tests the code for parrot_trouble.m')
 
 test_sameFirstLast
 
+disp('Tests the code for sameFirstLast.m')
+
 test_SleepIn
 
+disp('Tests the code for SleepIn.m')
+
 test_stringE
+
+disp('Tests the code for stringE.m')

7f519ad000c55653a0191d18ed59233d952c4175
Iain Crosby <ifcrosby@gmail.com>
2011-05-03 01:48:42 -0400

    upload

---
+++ nearOnes.m
@@ -0,0 +1,14 @@
+function y= nearOnes(a)
+% Given an int n, return true if it is within 10 of 100 or 200
+% Author: Iain Crosby
+% 4/29/11
+
+if a>=90 && a<=110
+y=true;
+elseif a>=190 && a<=210
+y=true;
+else
+y=false;
+
+end
+end
